2e2e50c1012ac1a2513a96050fcd6076d3d2ff64
Ivanov Vlad <lrn@2ch.hk>
2023-03-16 12:11:08 +0300

    initial commit

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,28 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean
+    public Animal dog() {
+        return new Dog();
+    }
+
+    @Bean
+    public AnimalsCage animalsCage() {
+        return new AnimalsCage();
+    }
+
+    @Bean
+    public Timer timer() {
+        return new Timer();
+    }
 }
